05998e16501792e2b41c510f4e886e194498c378
kathrynsteg <kathrynsteg@gmail.com>
2024-07-05 20:55:36 -0500

    Debugging - removed unused variables

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import City from "./City";
-import Temperature from "./Temperature";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
